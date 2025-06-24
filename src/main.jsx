d59bc2168e1fcc230a0d5c91104ff378b2472b65
Angelo Iaia <40299779-iamangeloiaia@users.noreply.replit.com>
2025-06-24 11:00:49 +0000

    fix: aggiunto bootstrap JS per navbar responsive

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import './index.css';
 import App from './App';
 import Login from './components/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 
 function ProtectedRoute({ children }) {
